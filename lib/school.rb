ffd7432d138f3b80030228b30a84efbf9fc0da46
Brock Byrd <brockbyrdd@gmail.com>
2020-09-16 18:34:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,17 +10,4 @@ class School
     @name = name
   end
 
-  def roster=(roster)
-    @roster = roster
-  end
-
-  def roster
-    @roster
-  end
-
-  def add_student(full_name, grade_level)
-    ROSTER << ROSTER[:grade_level] = full_name
-  end
-
-
 end
