85699e1ebb1dedfd94d1cdfbc6ed894200b8ea38
Brock Byrd <brockbyrdd@gmail.com>
2020-09-16 18:33:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+
+class School
+
+  attr_reader :name
+
+  ROSTER = {}
+  
+  def initialize(name)
+    @name = name
+  end
+
+  def roster=(roster)
+    @roster = roster
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(full_name, grade_level)
+    ROSTER << ROSTER[:grade_level] = full_name
+  end
+
+  
+end
